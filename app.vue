7cbd42858614af7a95534d8cb97a511d67767505
Mikhail <90400368+Innosan@users.noreply.github.com>
2024-02-05 19:36:21 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const appConfig = useAppConfig();
+
+onMounted(() => {
+	appConfig.isMobile = window.innerWidth <= 900;
+
+	window.addEventListener("resize", () => {
+		appConfig.isMobile = window.innerWidth <= 900;
+	});
+});
+
+onUnmounted(() => {
+	window.removeEventListener("resize", () => {
+		appConfig.isMobile = window.innerWidth <= 900;
+	});
+});
+</script>
+
+<template>
+	<div>
+		<Header />
+		<div class="page-container">
+			<NuxtPage />
+		</div>
+		<Footer />
+	</div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+	<h2>About page</h2>
+</template>
+
+<style scoped></style>
