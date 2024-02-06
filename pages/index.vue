c58f64631421dc567457d9da7bd2c2ed7d51be58
Innosan <reau.beua@gmail.com>
2024-02-07 01:30:44 +0300

    hydration error fix

--- pages/index.vue
+++ pages/index.vue
@@ -8,38 +8,43 @@ const currentLesson = currentDay;
 </script>
 
 <template>
-	<UTabs :items="scheduleTabs" class="mt-4">
-		<template
-			#default="{ item, index, selected }"
-			:class="selected ? 'bg-primary-500' : ''"
-		>
-			<div class="flex items-center gap-2 relative truncate">
-				<span class="font-bold">{{ item.label }}</span>
+	<ClientOnly>
+		<UTabs :items="scheduleTabs" class="mt-4">
+			<template
+				#default="{ item, index, selected }"
+				:class="selected ? 'bg-primary-500' : ''"
+			>
+				<div class="flex items-center gap-2 relative truncate">
+					<span class="font-bold">{{ item.label }}</span>
 
-				<span
-					v-if="selected"
-					class="absolute -right-4 w-2 h-2 rounded-full bg-primary-500 dark:bg-primary-400"
-				/>
-			</div>
-		</template>
-		<template #item="{ item: tabItem }">
-			<div v-if="tabItem.key === 'day'" class="mt-4">
-				<DayCard
-					:day="currentDay"
-					:index="getNumberOfDay()"
-					:show-day="false"
-				/>
-			</div>
-			<div v-else-if="tabItem.key === 'week'" class="flex flex-col gap-8">
-				<DayCard
-					v-for="(day, index) in currentSchedule.days"
-					:key="index"
-					:day="day"
-					:index="index"
-				/>
-			</div>
-		</template>
-	</UTabs>
+					<span
+						v-if="selected"
+						class="absolute -right-4 w-2 h-2 rounded-full bg-primary-500 dark:bg-primary-400"
+					/>
+				</div>
+			</template>
+			<template #item="{ item: tabItem }">
+				<div v-if="tabItem.key === 'day'" class="mt-4">
+					<DayCard
+						:day="currentDay"
+						:index="getNumberOfDay()"
+						:show-day="false"
+					/>
+				</div>
+				<div
+					v-else-if="tabItem.key === 'week'"
+					class="flex flex-col gap-8"
+				>
+					<DayCard
+						v-for="(day, index) in currentSchedule.days"
+						:key="index"
+						:day="day"
+						:index="index"
+					/>
+				</div>
+			</template>
+		</UTabs>
+	</ClientOnly>
 </template>
 
 <style scoped></style>
