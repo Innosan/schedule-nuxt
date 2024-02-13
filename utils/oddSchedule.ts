dbb2b711c0eba7078cd59bee7e69390b7485ed08
Innosan <reau.beua@gmail.com>
2024-02-13 23:03:28 +0300

    some tweaks and fixes new subjects page

--- utils/oddSchedule.ts
+++ utils/oddSchedule.ts
@@ -3,143 +3,7 @@ import { subjects } from "~/utils/subjects";
 import { lessonTypes } from "~/utils/lessonTypes";
 import { teachers } from "~/utils/teachers";
 
-export const firstSchedule: Schedule = {
-	type: "even",
-	days: [
-		// monday
-		[
-			{
-				subject: subjects[0],
-				type: lessonTypes.practice,
-				teachers: [teachers[16]],
-				room: "Аудитория физики №2 (за БФА)",
-			},
-			{
-				subject: subjects[1],
-				type: lessonTypes.lecture,
-				teachers: [teachers[1]],
-				room: "7-413",
-				additionalGroups: "4304, 4305, 4306",
-			},
-			{
-				subject: subjects[2],
-				type: lessonTypes.lecture,
-				teachers: [teachers[2]],
-				room: "301",
-				additionalGroups: "3 фак",
-			},
-			{},
-		],
-
-		// tuesday
-		[
-			{
-				subject: subjects[4],
-				type: lessonTypes.practice,
-				teachers: [teachers[8]],
-				room: "Кафедра физического воспитания",
-			},
-			{
-				subject: subjects[3],
-				type: lessonTypes.lecture,
-				teachers: [teachers[4]],
-				room: "215",
-				additionalGroups: "4306, 1 фак",
-			},
-			{
-				subject: subjects[1],
-				type: lessonTypes.practice,
-				teachers: [teachers[7]],
-				room: "7-120",
-			},
-			{
-				subject: subjects[6],
-				type: lessonTypes.practice,
-				teachers: [teachers[9]],
-				room: "7-321",
-			},
-		],
-
-		// wednesday
-		[
-			{
-				subject: subjects[0],
-				type: lessonTypes.lecture,
-				teachers: [teachers[0]],
-				room: "БФА",
-				additionalGroups: "4306",
-			},
-			{
-				subject: subjects[7],
-				type: lessonTypes.practice,
-				teachers: [teachers[10]],
-				room: "306",
-			},
-			{
-				subject: subjects[5],
-				type: lessonTypes.practice,
-				teachers: [teachers[3]],
-				room: "САПРиУ",
-			},
-			{},
-		],
-
-		// thursday
-		[
-			{
-				subject: subjects[2],
-				type: lessonTypes.practice,
-				teachers: [teachers[2]],
-				room: "259",
-			},
-			{
-				subject: subjects[7],
-				type: lessonTypes.practice,
-				teachers: [teachers[10]],
-				room: "394",
-			},
-			{
-				subject: subjects[8],
-				type: lessonTypes.lecture,
-				teachers: [teachers[12]],
-				room: "СА",
-				additionalGroups: "4306",
-			},
-			{
-				subject: subjects[9],
-				type: lessonTypes.practice,
-				teachers: [teachers[14], teachers[13]],
-				room: "7",
-			},
-		],
-
-		// friday
-		[
-			{
-				subject: subjects[7],
-				type: lessonTypes.lecture,
-				teachers: [teachers[11]],
-				room: "211",
-				additionalGroups: "5 фак",
-			},
-			{
-				subject: subjects[8],
-				type: lessonTypes.practice,
-				teachers: [teachers[12]],
-				room: "СА",
-			},
-			{},
-			{
-				subject: subjects[4],
-				type: lessonTypes.practice,
-				teachers: [teachers[8]],
-				room: "Кафедра физического воспитания",
-			},
-		],
-	],
-};
-
-export const secondSchedule: Schedule = {
+export const oddSchedule: Schedule = {
 	type: "odd",
 	days: [
 		// monday
@@ -147,7 +11,7 @@ export const secondSchedule: Schedule = {
 			{
 				subject: subjects[0],
 				type: lessonTypes.labWork,
-				teachers: [teachers[15], teachers[16]],
+				teachers: [teachers[14], teachers[15]],
 				room: "Аудитория кафедры",
 			},
 			{
@@ -244,14 +108,14 @@ export const secondSchedule: Schedule = {
 			{
 				subject: subjects[8],
 				type: lessonTypes.lecture,
-				teachers: [teachers[12]],
+				teachers: [teachers[6]],
 				room: "СА",
 				additionalGroups: "4306",
 			},
 			{
 				subject: subjects[9],
 				type: lessonTypes.practice,
-				teachers: [teachers[13], teachers[14]],
+				teachers: [teachers[12], teachers[13]],
 				room: "7",
 			},
 		],
@@ -268,7 +132,7 @@ export const secondSchedule: Schedule = {
 			{
 				subject: subjects[8],
 				type: lessonTypes.practice,
-				teachers: [teachers[12]],
+				teachers: [teachers[6]],
 				room: "СА",
 			},
 			{
