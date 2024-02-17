c0f985082e1a8194e1caaaea6c93339bfc005fcf
Innosan <reau.beua@gmail.com>
2024-02-17 14:38:34 +0300

    new timer and app icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,5 +15,34 @@ export default defineNuxtConfig({
 		preference: "dark",
 	},
 
+	app: {
+		head: {
+			title: "Schedule",
+			link: [
+				{
+					rel: "apple-touch-icon",
+					sizes: "180x180",
+					href: "/apple-touch-icon.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "32x32",
+					href: "/favicon-32x32.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "16x16",
+					href: "/favicon-16x16.png",
+				},
+				{
+					rel: "manifest",
+					href: "/site.webmanifest",
+				},
+			],
+		},
+	},
+
 	css: ["~/assets/styles/main.css"],
 });
